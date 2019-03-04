4d1ab58df0dfbd9fa4da1a15f74c1a3bfa692b13
nativelocation <bluesky6460@mail.com>
2019-03-04 04:15:09 -0500

    update webpack for importing font

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
         ],
       },
       {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader'
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
