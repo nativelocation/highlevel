799b10be608e0352d340d54614cc7ab3564ac1f6
nativelocation <bluesky6460@mail.com>
2019-03-04 04:19:44 -0500

    integrate scss with bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
+import 'bootstrap'
+
 import App from './App.vue';
 import router from './router';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/scss/index.scss';
 
 new Vue({
 	el: '#app',
