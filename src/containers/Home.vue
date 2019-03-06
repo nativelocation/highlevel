97deb2c9fc411fb556f9454b081e5c35b5c675a7
nativelocation <bluesky6460@mail.com>
2019-03-06 03:14:11 -0500

    update home

--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -1,14 +1,17 @@
 <template>
 	<div id="home">
 		<NavBar />
+		<Header />
 	</div>
 </template>
 
 <script>
 import NavBar from '../components/Navbar'
+import Header from '../components/Header'
 export default {
 	components: {
 		NavBar,
+		Header,
 	},
 }
 </script>
